0bde656327665db52d88b29d68272844b11f166e
veematics <viktor.iwan@doxadigital.com>
2025-04-04 19:56:20 +0700

    rebase

---
+++ resources/js/sidebar-handler.js
@@ -0,0 +1,29 @@
+const initSidebarHandler = () => {
+    const sidebar = document.getElementById('sidebar');
+    const dashboard = document.querySelector('.app-dashboard');
+
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.attributeName === 'class') {
+                const isFolded = sidebar.classList.contains('sidebar-narrow') || 
+                               sidebar.classList.contains('sidebar-narrow-unfoldable');
+                
+                requestAnimationFrame(() => {
+                    if (isFolded) {
+                        dashboard.classList.remove('px-4');
+                        dashboard.style.maxWidth = '90%';
+                    } else {
+                        dashboard.classList.add('px-4');
+                        dashboard.style.removeProperty('max-width');
+                    }
+                });
+            }
+        });
+    });
+
+    observer.observe(sidebar, {
+        attributes: true
+    });
+};
+
+export { initSidebarHandler };
---
+++ resources/views/services/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card mb-4 mx-4">
+                        <div class="card-body p-4">
+                            <h1 class="text-center mb-0">Our Services {{Doxadigital}}<h1>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
