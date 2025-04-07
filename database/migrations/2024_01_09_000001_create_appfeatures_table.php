9442b81843ebb162d0ab2570d93c88c6e48f0c21
veematics <viktor.iwan@doxadigital.com>
2025-04-07 19:15:15 +0700

    Fix migrate: Create App Features table

--- database/migrations/2024_01_09_000001_create_appfeatures_table.php
+++ database/migrations/2024_01_09_000001_create_appfeatures_table.php
@@ -8,13 +8,26 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('appfeatures', function (Blueprint $table) {
-            $table->id('featureID');
-            $table->string('featureName');
-            $table->string('featureIcon');
-            $table->string('featurePath');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('appfeatures')) {
+            Schema::create('appfeatures', function (Blueprint $table) {
+                $table->id('featureID');
+                $table->string('featureName');
+                $table->string('featureIcon');
+                $table->string('featurePath');
+                $table->boolean('featureActive')->default(true);
+                $table->text('custom_permission')->nullable();
+                $table->timestamps();
+            });
+        } else {
+            Schema::table('appfeatures', function (Blueprint $table) {
+                if (!Schema::hasColumn('appfeatures', 'featureActive')) {
+                    $table->boolean('featureActive')->default(true);
+                }
+                if (!Schema::hasColumn('appfeatures', 'custom_permission')) {
+                    $table->text('custom_permission')->nullable();
+                }
+            });
+        }
     }
 
     public function down(): void
